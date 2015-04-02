6e58a691c3a8edb2e6831e589940e19dd54200c5
Alex Sears <alexwsears@gmail.com>
2015-04-01 21:35:01 -0400

    Added new gem backbone

---
+++ lib/venn/email/ruby.rb
@@ -0,0 +1,9 @@
+require "venn/email/ruby/version"
+
+module Venn
+  module Email
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/venn/email/ruby/version.rb
@@ -0,0 +1,7 @@
+module Venn
+  module Email
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
